1d4f87bb02a173da283dc04289d0fb4ac8f0f259
Łukasz Kurczab <lukasz.kurczab@capgemini.com>
2023-10-09 12:20:45 +0200

    Exercise mapper

--- src/screens/atlasScreen/atlasScreen.tsx
+++ src/screens/atlasScreen/atlasScreen.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
 import { View, Text } from 'react-native';
 import styles from './atlasScreen.styles';
 import ExerciseTile from '../../components/tiles/exerciseTile/exerciseTile';
 import AtlasDropdown from '../../components/atlasDropdown/atlasDropdown';
-import { store } from '../../store/store';
+import { Exercise } from '../../types/exercise';
 
 const AtlasScreen = () => {
+  const { exercises } = useSelector((store: RootState) => store.exercises);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Exercise atlas</Text>
       <AtlasDropdown/>
       <View style={styles.articlesWrapper}>
-        <ExerciseTile/>
+        {exercises.map((exercise: Exercise) => (
+          <ExerciseTile {...exercise} />
+        ))}
       </View>
     </View>
   );
