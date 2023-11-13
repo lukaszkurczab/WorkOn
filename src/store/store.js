435af5d4d5cf49f81a78b1e5dd66a4d3a105b5c7
Łukasz Kurczab <lukasz.kurczab@gmail.com>
2023-11-13 20:33:52 +0100

    store modified

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
 import exerciseReducer from './reducers/exerciseReducer';
 import planReducer from './reducers/planReducer';
 
+const rootReducer = combineReducers({ 
+  exercises: exerciseReducer,
+  plans: planReducer
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const store = configureStore({
-  reducer: {
-    exercises: exerciseReducer,
-    plans: planReducer
-  }
+  reducer: persistedReducer,
+  middleware: [thunk]
 });
+
+export const persistor = persistStore(store)
