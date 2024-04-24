227055c975b67bd7adb6d13b29cac10a69874ffa
Łukasz Kurczab <lukasz.kurczab@gmail.com>
2024-04-24 16:56:28 +0200

    getExercises fix

--- src/api/exercises.js
+++ src/api/exercises.js
@@ -1,12 +1,6 @@
 export const fetchExercises = async version => {
   try {
-    const response = await fetch('https://workon-backend.azurewebsites.net/exercises', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ version: '1.0' }),
-    });
+    const response = await fetch('https://workon-backend.azurewebsites.net/exercises/1.0');
     const data = await response.json();
     return data;
   } catch (error) {
